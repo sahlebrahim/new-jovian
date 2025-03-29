ea876f437502d0252a5dc4bb008bf90429fb8759
sahlebrahim <sahlebrahim@gmail.com>
2025-03-29 18:13:52 +0400

    added pages foe jobs

--- app.py
+++ app.py
@@ -1,26 +1,29 @@
-from flask import Flask,render_template
-from database import engine
+from flask import Flask,render_template,jsonify
+from database import engine,load_jobs,load_job_id
 from sqlalchemy import text
 
 app = Flask(__name__)
 
 
 
-def load_jobs():
-    with engine.connect() as conn:
-      result = conn.execute(text("select * from jobs"))
-      result_dicts = []
-      for row in result.all():
-          #The ._mapping attribute in SQLAlchemy Row objects provides dictionary-like access to the data in a database row.
-          result_dicts.append(dict(row._mapping))
-      
-    return result_dicts
+
 
 
 @app.route("/")
 def hello_world():
     jobs = load_jobs()
     return render_template("home.html",jobs = jobs)
+
+
+@app.route("/job/<id>")
+def job(id):
+    job = load_job_id(id)
+    if not job:
+        return "Job not found", 404
+    
+    #print(job)
+    return render_template("jobpage.html",job = job)
+
 
 if __name__=="__main__":
     app.run(host='0.0.0.0',debug=True)
--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 import os
 from dotenv import load_dotenv
+from sqlalchemy import text
 load_dotenv()
 db_string=os.environ["DB_CONNECTION_STRING"]
 
@@ -13,4 +14,24 @@ engine = create_engine(
     },
 
 )
+
+def load_jobs():
+    with engine.connect() as conn:
+      result = conn.execute(text("select * from jobs"))
+      result_dicts = []
+      for row in result.all():
+          #The ._mapping attribute in SQLAlchemy Row objects provides dictionary-like access to the data in a database row.
+          result_dicts.append(dict(row._mapping))
+      
+    return result_dicts
+
+def load_job_id(id):
+   with engine.connect() as conn:
+      result = conn.execute(text("select * from jobs where id = :val"), {"val": id})
+      rows = result.all()
+      if len(rows) == 0:
+          return None
+      else:
+          return dict(rows[0]._mapping)
+   
 
